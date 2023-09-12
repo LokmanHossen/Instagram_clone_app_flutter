2dda84387893271cce7740f3328521516ad58197
LokmanHossen <lokmanhossen230@gmail.com>
2023-09-12 12:28:44 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone_app/onbordaring.dart';
+// import 'package:instagram_clone_app/onbordaring.dart';
+import 'package:instagram_clone_app/sign_up.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnBordaring(),
+      // home: OnBordaring(),
+      home: SignUpScreen(),
     );
   }
 }
