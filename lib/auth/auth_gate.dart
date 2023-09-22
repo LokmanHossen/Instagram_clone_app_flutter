ec109c7cba4429a9bc54ecf357b7b8aedba18ae0
LokmanHossen <lokmanhossen230@gmail.com>
2023-09-22 22:46:15 +0600

    Firebase Auth connection

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_app/auth/sign_up.dart';
+import 'package:instagram_clone_app/body_view.dart';
 
 class AuthGate extends StatefulWidget {
   const AuthGate({super.key});
@@ -8,8 +11,32 @@ class AuthGate extends StatefulWidget {
 }
 
 class _AuthGateState extends State<AuthGate> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 0), () {
+      checkAuthState();
+    });
+    super.initState();
+  }
+
+  void checkAuthState() {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: ((context) => const Bodyview())));
+    } else {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: ((context) => const SignUpScreen())));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
