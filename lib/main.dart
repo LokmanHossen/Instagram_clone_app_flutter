fca3e5fa036eb3ff8fe21f8934e6dc2ca0edc250
LokmanHossen <lokmanhossen230@gmail.com>
2023-09-25 00:00:28 +0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_clone_app/auth/auth_gate.dart';
+import 'package:instagram_clone_app/prensentation/pages/auth/auth_gate.dart';
 
 import 'firebase_options.dart';
 
