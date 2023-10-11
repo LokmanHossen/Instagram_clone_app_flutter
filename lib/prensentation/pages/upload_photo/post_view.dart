8ef9cad4e27857a4dd76062ea80b54ed139956b0
LokmanHossen <lokmanhossen230@gmail.com>
2023-10-11 23:36:10 +0600

    AddPost

--- lib/prensentation/pages/upload_photo/post_view.dart
+++ lib/prensentation/pages/upload_photo/post_view.dart
@@ -1,151 +1,11 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_app/prensentation/pages/upload_photo/widgets/media_picker.dart';
 import 'package:instagram_clone_app/prensentation/pages/upload_photo/widgets/post_bottom_sheet.dart';
-
-// class PostView extends StatefulWidget {
-//   const PostView({super.key});
-
-//   @override
-//   State<PostView> createState() => _PostViewState();
-// }
-
-// class _PostViewState extends State<PostView> {
-//   var mediaPicker = MediaPicker();
-
-//   final List<Map<String, dynamic>> _mediaFiles = [];
-
-//   void pickImgaes() async {
-//     var pickFiles = await mediaPicker.pickImage();
-//     if (pickFiles != null) {
-//       setState(() {
-//         _mediaFiles.addAll(pickFiles);
-//       });
-//     }
-//     Navigator.pop(context);
-//   }
-
-//   Future<void> takePicture() async {
-//     var pickFile = await mediaPicker.takePicture();
-//     if (pickFile != null) {
-//       setState(() {
-//         _mediaFiles.add(pickFile);
-//       });
-//     }
-//     Navigator.pop(context);
-//   }
-
-//   Future<void> pickVideo() async {
-//     var pickFile = await mediaPicker.videoPick();
-//     if (pickFile != null) {
-//       setState(() {
-//         _mediaFiles.add(pickFile);
-//       });
-//     }
-//     Navigator.pop(context);
-//   }
-
-//   removeMedia(index) {
-//     setState(() {
-//       _mediaFiles.removeAt(index - 1);
-//     });
-//     Navigator.pop(context);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.black,
-//       floatingActionButton: ElevatedButton(
-//           style: ElevatedButton.styleFrom(
-//               backgroundColor: const Color.fromARGB(255, 255, 66, 53)),
-//           onPressed: () {},
-//           child: const Text("Post")),
-//       appBar: AppBar(title: const Text("Upload Post")),
-//       body: ListView(
-//         children: [
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 const Text("Write Something"),
-//                 const SizedBox(
-//                   height: 10,
-//                 ),
-//                 TextField(
-//                   maxLines: null,
-//                   keyboardType: TextInputType.multiline,
-//                   decoration: InputDecoration(
-//                       border: OutlineInputBorder(
-//                         borderRadius: BorderRadius.circular(8),
-//                         borderSide: BorderSide.none,
-//                       ),
-//                       contentPadding: const EdgeInsets.symmetric(
-//                           horizontal: 8, vertical: 8),
-//                       hintText: 'Description',
-//                       filled: true,
-//                       fillColor: const Color.fromARGB(255, 53, 53, 53)),
-//                 )
-//               ],
-//             ),
-//           ),
-//           const SizedBox(
-//             height: 10,
-//           ),
-//           GridView.builder(
-//               shrinkWrap: true,
-//               physics: const NeverScrollableScrollPhysics(),
-//               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//                 crossAxisCount: 3,
-//                 crossAxisSpacing: 10,
-//                 mainAxisSpacing: 10,
-//               ),
-//               itemCount: _mediaFiles.length + 1,
-//               itemBuilder: (BuildContext context, int index) {
-//                 if (index == 0) {
-//                   return Container(
-//                     decoration: BoxDecoration(
-//                         border: Border.all(color: Colors.grey),
-//                         borderRadius: BorderRadius.circular(8.0)),
-//                     child: IconButton(
-//                         onPressed: () {
-//                           MediaBottomSheet.show(
-//                               context, pickImgaes, takePicture, pickVideo);
-//                         },
-//                         icon: const Icon(Icons.add_a_photo)),
-//                   );
-//                 }
-//                 var mediaFile = _mediaFiles[index - 1];
-//                 var thumbnailFile = File(mediaFile['thumbnailFile']);
-//                 var isVideo = mediaFile['mediaType'] == 'video';
-//                 return InkWell(
-//                   onLongPress: () {
-//                     RemoveMediaBottomSheet.show(context, () {
-//                       removeMedia(index);
-//                     });
-//                   },
-//                   child: Stack(
-//                     children: [
-//                       Image.file(thumbnailFile,
-//                           width: double.infinity,
-//                           height: double.infinity,
-//                           fit: BoxFit.cover),
-//                       if (isVideo)
-//                         const Align(
-//                             alignment: Alignment.center,
-//                             child: Icon(Icons.play_circle_filled,
-//                                 size: 40, color: Colors.white))
-//                     ],
-//                   ),
-//                 );
-//               }),
-//         ],
-//       ),
-//     );
-//   }
-// }
+import 'package:instagram_clone_app/services/firestore_service.dart';
+import 'package:uuid/uuid.dart';
 
 class PostView extends StatefulWidget {
   const PostView({super.key});
@@ -156,8 +16,11 @@ class PostView extends StatefulWidget {
 
 class _PostViewState extends State<PostView> {
   var mediaPicker = MediaPicker();
-
-  final List<Map<String, dynamic>> _mediaFiles = [];
+  List<Map<String, dynamic>> _mediaFiles = [];
+  final _desController = TextEditingController();
+  var imageUrlList = [];
+  var isLoader = false;
+  final firestoreService = FirestoreService();
 
   void pickImgaes() async {
     Navigator.pop(context);
@@ -190,6 +53,54 @@ class _PostViewState extends State<PostView> {
     }
   }
 
+  var uuid = const Uuid();
+  uploadFile() async {
+    for (var i in _mediaFiles) {
+      var imageUrl =
+          await firestoreService.uploadFile(File(i['thumbnailFile']));
+      setState(() {
+        imageUrlList.add(imageUrl);
+      });
+    }
+  }
+
+  uploadPost() async {
+    setState(() {
+      isLoader = false;
+    });
+    await uploadFile();
+    var id = uuid.v4();
+    var createAt = DateTime.now().microsecondsSinceEpoch;
+
+    var postData = {
+      "id": id,
+      "userId": FirebaseAuth.instance.currentUser!.uid,
+      "createAt": createAt,
+      "linksImages": [],
+      "commentsCount": 0,
+      "likesCouont": 0,
+      "description": _desController.text,
+      "postImages": imageUrlList,
+    };
+    try {
+      await firestoreService.addPost(postData);
+      setState(() {
+        isLoader = true;
+      });
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Post Uploaded')));
+    } catch (error) {
+      setState(() {
+        isLoader = false;
+      });
+    } finally {
+      setState(() {
+        _desController.clear();
+        _mediaFiles = [];
+      });
+    }
+  }
+
   removeMedia(index) {
     setState(() {
       _mediaFiles.removeAt(index - 1);
@@ -201,12 +112,23 @@ class _PostViewState extends State<PostView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      floatingActionButton: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: const Color.fromARGB(255, 255, 66, 53)),
-          onPressed: () {},
-          child: Text("Post")),
-      appBar: AppBar(title: Text("Upload Post")),
+      floatingActionButton: SizedBox(
+        height: 40,
+        width: 90,
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 255, 66, 53)),
+            onPressed: () {
+              if (isLoader) {
+              } else {
+                uploadPost();
+              }
+            },
+            child: isLoader
+                ? const Center(child: CircularProgressIndicator())
+                : const Text("Post")),
+      ),
+      appBar: AppBar(title: const Text("Upload Post")),
       body: ListView(
         children: [
           Padding(
@@ -214,30 +136,31 @@ class _PostViewState extends State<PostView> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Write Something"),
-                SizedBox(
+                const Text("Write Something"),
+                const SizedBox(
                   height: 10,
                 ),
                 TextField(
                   maxLines: null,
+                  controller: _desController,
                   keyboardType: TextInputType.multiline,
                   decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                         borderSide: BorderSide.none,
                       ),
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 8),
                       hintText: 'Description',
                       filled: true,
-                      fillColor: Color.fromARGB(255, 53, 53, 53)),
+                      fillColor: const Color.fromARGB(255, 53, 53, 53)),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 GridView.builder(
                     shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
+                    physics: const NeverScrollableScrollPhysics(),
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 3,
@@ -256,7 +179,7 @@ class _PostViewState extends State<PostView> {
                                 MediaBottomSheet.show(context, pickImgaes,
                                     takePicture, pickVideo);
                               },
-                              icon: Icon(Icons.add_a_photo)),
+                              icon: const Icon(Icons.add_a_photo)),
                         );
                       }
                       var mediaFile = _mediaFiles[index - 1];
@@ -275,7 +198,7 @@ class _PostViewState extends State<PostView> {
                                 height: double.infinity,
                                 fit: BoxFit.cover),
                             if (isVideo)
-                              Align(
+                              const Align(
                                   alignment: Alignment.center,
                                   child: Icon(Icons.play_circle_filled,
                                       size: 40, color: Colors.white))
