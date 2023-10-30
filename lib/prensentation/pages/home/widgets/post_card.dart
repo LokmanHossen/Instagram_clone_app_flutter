af871cd63a7252a95a091201e45ae8e327d62e07
LokmanHossen <lokmanhossen230@gmail.com>
2023-10-31 03:33:12 +0600

    Update

--- lib/prensentation/pages/home/widgets/post_card.dart
+++ lib/prensentation/pages/home/widgets/post_card.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_app/prensentation/pages/home/widgets/post_header.dart';
 import 'package:instagram_clone_app/prensentation/pages/home/widgets/post_images.dart';
 
-import 'like_images.dart';
+import 'post_footer.dart';
 
 class PostCard extends StatelessWidget {
   const PostCard({
@@ -11,10 +12,11 @@ class PostCard extends StatelessWidget {
   });
 
   final List<String> likeImagesList;
-  final dynamic postData;
+  final Map<String, dynamic> postData;
 
   @override
   Widget build(BuildContext context) {
+    var postImages = postData['postImages'] as List ?? [];
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
@@ -25,7 +27,10 @@ class PostCard extends StatelessWidget {
           const SizedBox(
             height: 15,
           ),
-          PostImages(),
+          if (postImages.isNotEmpty)
+            PostImages(
+              imageUrls: postImages,
+            ),
           // ClipRRect(
           //   borderRadius: BorderRadius.circular(10),
           //   child: Image.network(
@@ -38,77 +43,12 @@ class PostCard extends StatelessWidget {
           const SizedBox(
             height: 15,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  LikesImages(
-                    likeImages: likeImagesList,
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  const Text("15 Likes",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Icon(Icons.favorite_border_outlined),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  const Icon(Icons.message_outlined),
-                ],
-              ),
-              const Icon(Icons.bookmark_border_outlined),
-            ],
+          PostFooter(
+            likeImagesList: likeImagesList,
+            postData: postData,
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            "View All 48 comments",
-            style: TextStyle(color: Colors.grey),
-          )
         ],
       ),
     );
-  }
-}
-
-class PostHeader extends StatelessWidget {
-  const PostHeader({
-    super.key,
-    required this.postData,
-  });
-
-  final dynamic postData;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-      Row(children: [
-        CircleAvatar(
-          radius: 35,
-          backgroundImage: NetworkImage(postData['profileImage']),
-        ),
-        const SizedBox(
-          width: 10,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              postData['userName'],
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-            ),
-            const Text('15 mins ago', style: TextStyle(color: Colors.white38))
-          ],
-        )
-      ]),
-      const Icon(Icons.more_vert)
-    ]);
   }
 }
